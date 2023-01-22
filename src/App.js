157ce13c273af5b219711ac3221464afd6c1c4dd
Ahmed-Raza-Khanzada <khanzadaahmedraza@gmail.com>
2023-01-22 16:38:13 +0500

    features update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Header from './components/Header';
 import Pricing from './components/Pricing';
+import {Features} from './components/Features';
+
 
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
 <div className="App">
    <Header />
   <Pricing/>
+  <Features/>
 </div>
   );
 }
